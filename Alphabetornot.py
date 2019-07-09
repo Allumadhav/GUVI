4f6eb7064459fe6e695113e4038c39698643f2d0
ALLU MADHAV <36040020+Allumadhav@users.noreply.github.com>
2019-07-09 21:55:56 +0530

    Add files via upload

---
+++ Alphabetornot.py
@@ -0,0 +1,5 @@
+giveaninput = input()
+if giveaninput in ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','a','e','i','o','u'):
+    print("Alphabet")
+else:
+    print("No")
