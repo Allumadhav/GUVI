323ac8634abb32cb23e52f8bffc1f3b9d2b4a293
ALLU MADHAV <36040020+Allumadhav@users.noreply.github.com>
2019-07-09 20:42:58 +0530

    even or odd number

---
+++ even or odd.py
@@ -0,0 +1,9 @@
+enterednumberis = int(input())
+if enterednumberis >=0:
+    
+    if enterednumberis % 2 == 0:
+        print("Even")
+    elif enterednumberis % 2 == 1:
+        print("Odd")
+else:
+    print("invalid")
